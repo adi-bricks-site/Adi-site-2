cd97e4209c71b1ec5bcd3875cbbccfd8628a6a80
MSM <msm74588@gmail.com>
2022-09-17 22:36:19 +0530

    updated netlify config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
+import netlify from '@astrojs/netlify/functions';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind({
